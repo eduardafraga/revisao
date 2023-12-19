df64af2b039d7d3ee19629ea567c81820917883a
Eduarda Teixeira <145463261+eduardafraga@users.noreply.github.com>
2023-12-19 09:52:27 -0300

    Add files via upload

---
+++ aula01/exp03.php
@@ -0,0 +1,12 @@
+<html>
+
+<?php
+$color= "red";
+
+echo "My car is" . $color . "<br>";
+echo "My house is" . $color . "<br>";
+echo "My car is" . $color . "<br>";
+?>
+
+
+</html>
---
+++ aula01/exp04.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+eCHo "hello world<br>";
+echo "hello world <br>";
+EcHo "hello world <br>";
+
+?>
+
+</html>
