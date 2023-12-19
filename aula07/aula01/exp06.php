359134e7007ec96030979d98443e611f4b41870d
Eduarda Teixeira <145463261+eduardafraga@users.noreply.github.com>
2023-12-19 09:51:31 -0300

    Add files via upload

---
+++ aula07/aula01/exp06.php
@@ -0,0 +1,8 @@
+<!DOCTYPE hmtl>
+<html>
+
+<?php
+$txt = "escolas";
+echo ="eu amo $txt";
+?>
+</html>
---
+++ aula07/aula01/index.php
@@ -0,0 +1,14 @@
+<?php
+ echo 'teste impressao php';
+
+ echo '<br>';
+ echo '<hr>';
+ echo '<p>';
+
+?>
+<?= "mais uma nova linha agora" ?>
+
+<?php
+
+// echo "sla" ;
+
