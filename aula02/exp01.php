28b08318d4b94b1a7c6ee2a51faefdd2cb46861c
Eduarda Teixeira <145463261+eduardafraga@users.noreply.github.com>
2023-12-19 09:52:55 -0300

    Add files via upload

---
+++ aula02/exp01.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1> VSCODE cor lilas</h1>
+    <p class="txt1"> VSCODE cor vermelho</p>
+    <h2> Nnn cor fucsia</h2>
+    <p class="txt2"> sabor vinho cor vinho</p>
+</body>
+</html>
